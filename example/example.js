e89976a5c72d7093accd71feb9a590bf6017337b
vq0599 <vq0599@outlook.com>
2018-06-10 16:02:16 +0800

    simplify example

---
+++ example/example.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/* eslint-disable */
+class Test extends React.Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    )
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'))

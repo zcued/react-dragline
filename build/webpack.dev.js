b93186d14ccde023e7fff316a9b18cca13000d86
vq0599 <vq0599@outlook.com>
2018-06-10 16:03:05 +0800

    ADD: webpack config

---
+++ build/webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  entry: './example/example.js',
+  output: {
+    path: path.resolve('./example'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+          'eslint-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: '8013',
+    open: true,
+    contentBase: './example',
+  },
+}

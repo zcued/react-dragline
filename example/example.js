14a89911bfaacc354afea063c1aaa623de32f376
vq0599 <vq0599@outlook.com>
2018-06-11 13:56:37 +0800

    ADD: example

--- example/example.js
+++ example/example.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { DraggableContainer, DraggableChild } from '../src/index'
 
-/* eslint-disable */
-class Test extends React.Component {
+class Example extends React.Component {
+  children = [
+    { id: 1, color: '#0000FF', size: 50 },
+    { id: 2, color: '#00FF99', size: 100 },
+    { id: 3, color: '#333366', size: 150 },
+    { id: 4, color: '#CC66FF', size: 200 },
+  ]
+
+  _renderChildren() {
+    return this.children.map(({ id, color, size }) => {
+      const style = { background: color, width: size, height: size }
+      return (
+        <DraggableChild key={id}>
+          <div style={style} />
+        </DraggableChild>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>Hello world</div>
+      <DraggableContainer>
+        {this._renderChildren()}
+      </DraggableContainer>
     )
   }
 }
 
-ReactDOM.render(<Test />, document.getElementById('root'))
+ReactDOM.render(<Example />, document.getElementById('root'))

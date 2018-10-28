d4824cf6f6a5eda977fa7b8ff5693874b156bc36
huangdehao <huangdehao@zcool.com.cn>
2018-10-28 15:29:47 +0800

    Feature: calc align element

--- example/example.js
+++ example/example.js
@@ -7,8 +7,8 @@ class Example extends React.Component {
   children = [
     { id: 1, color: '#0000FF', size: 50 },
     { id: 2, color: '#00FF99', size: 100 },
-    { id: 3, color: '#333366', size: 150 },
-    { id: 4, color: '#CC66FF', size: 200 },
+    // { id: 3, color: '#333366', size: 150 },
+    // { id: 4, color: '#CC66FF', size: 200 },
   ]
 
   render() {

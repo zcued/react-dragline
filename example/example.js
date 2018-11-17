185b7555d02315624b849aa2db7ce4a26fd78cac
huangdehao <huangdehao@zcool.com.cn>
2018-11-17 18:07:54 +0800

    update example

--- example/example.js
+++ example/example.js
@@ -5,11 +5,11 @@ import { DraggableContainer, DraggableChild } from '../src/index'
 
 
 const initialChildren = [
-  { id: 1, color: '#0000FF', size: 100, position: {x: 100, y: 0} },
-  { id: 2, color: '#0000FF', size: 100, position: {x: 200, y: 106} },
-  { id: 3, color: '#00FF99', size: 102, position: {x: 500, y: 106} },
-  { id: 4, color: '#333366', size: 150, position: {x: 100, y: 316} },
-  { id: 5, color: '#CC66FF', size: 200, position: {x: 480, y: 376} },
+  { id: 1, background: '#0000FF', size: 100, position: {x: 100, y: 0} },
+  { id: 2, background: '#0000FF', size: 100, position: {x: 200, y: 106} },
+  { id: 3, background: '#00FF99', size: 102, position: {x: 500, y: 106} },
+  { id: 4, background: '#333366', size: 150, position: {x: 100, y: 316} },
+  { id: 5, background: '#CC66FF', size: 200, position: {x: 480, y: 376} },
 ]
 
 class Example extends React.Component {
@@ -29,12 +29,23 @@ class Example extends React.Component {
     return (
       <DraggableContainer style={{height: 600, border: '1px solid #ddd'}}>
         {
-          this.state.children.map(({ id, color, size, position }, index) => {
-            const style = { background: color, width: size, height: size }
+          this.state.children.map(({ id, background, size, position }, index) => {
+            const style = {
+              background,
+              width: size,
+              height: size,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }
 
             return (
               <DraggableChild key={id} position={position} handleChange={position => this.handleChange(index, position)}>
-                <div style={style}>size: {size}</div>
+                <div style={style}>
+                  <span>size: {size}</span>
+                  <span>drag me</span>
+                </div>
               </DraggableChild>
             )
           })

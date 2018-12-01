40806a439b2ecbd55823c74bc5fae095a4149303
vq0599 <vq0599@outlook.com>
2018-12-01 23:41:34 +0800

    develop: optimize example

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ import { DraggableContainer, DraggableChild } from './src/index'
 
 
 const initialChildren = [
-  { id: 1, background: '#8ce8df', size: 100, position: {x: 100, y: 0} },
-  { id: 2, background: '#8ce8df', size: 100, position: {x: 200, y: 106} },
-  { id: 3, background: '#afc7fd', size: 102, position: {x: 500, y: 106} },
+  { id: 1, background: '#8ce8df', size: 100, position: {x: 100, y: 10} },
+  { id: 2, background: '#8ce8df', size: 100, position: {x: 400, y: 106} },
   { id: 4, background: '#d2aff6', size: 150, position: {x: 100, y: 316} },
   { id: 5, background: '#fee493', size: 200, position: {x: 480, y: 376} },
 ]
@@ -35,31 +34,34 @@ class Example extends React.Component {
     }
 
     return (
-      <DraggableContainer style={containerStyle}>
-        {
-          this.state.children.map(({ id, background, size, position }, index) => {
-            const style = {
-              background,
-              width: size,
-              height: size,
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              cursor: 'move',
-            }
+      <div>
+        <style>{'.active { opacity: .5; }'}</style>
+        <DraggableContainer style={containerStyle}>
+          {
+            this.state.children.map(({ id, background, size, position }, index) => {
+              const style = {
+                background,
+                width: size,
+                height: size,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                cursor: 'move',
+              }
 
-            return (
-              <DraggableChild key={id} position={position} handleChange={position => this.handleChange(index, position)}>
-                <div className="item" style={style}>
-                  <span>size: {size}</span>
-                  <span>drag me</span>
-                </div>
-              </DraggableChild>
-            )
-          })
-        }
-      </DraggableContainer>
+              return (
+                <DraggableChild key={id} position={position} handleChange={position => this.handleChange(index, position)}>
+                  <div className="item" style={style}>
+                    <span>size: {size}</span>
+                    <span>drag me</span>
+                  </div>
+                </DraggableChild>
+              )
+            })
+          }
+        </DraggableContainer>
+      </div>
     )
   }
 }

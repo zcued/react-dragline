03e8b7fb9908a712ad902ebdc055903ab833cf4e
huangdehao <huangdehao@zcool.com.cn>
2018-12-15 19:08:04 +0800

    Format

--- build/webpack.config.docs.js
+++ build/webpack.config.docs.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 
 module.exports = {
-  entry:  path.resolve('./index.js'),
+  entry: path.resolve('./index.js'),
   output: {
     path: path.resolve('./docs'),
     filename: 'bundle.js',

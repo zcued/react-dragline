b541f5eae05b56725a138696b822591741599c40
huangdehao <huangdehao@zcool.com.cn>
2018-12-15 18:59:11 +0800

    develop for uncontroll mode

--- src/DraggableChild.js
+++ src/DraggableChild.js
@@ -2,43 +2,62 @@ import React from 'react'
 import { DraggableCore } from 'react-draggable'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import { createCoreData, noop } from './utils'
 
 
 export default class DraggableChild extends React.Component {
   static propTypes = {
     children: PropTypes.element.isRequired,
-    position: PropTypes.object,
+    defaultPosition: PropTypes.object,
+    onStart: PropTypes.func,
+    onDrag: PropTypes.func,
+    onStop: PropTypes.func,
   }
 
   static defaultProps = {
-    position: {x: 0, y: 0},
+    defaultPosition: { x: 0, y: 0 },
+    onStart: noop,
+    onDrag: noop,
+    onStop: noop,
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      x: props.position.x,
-      y: props.position.y,
+      x: props.defaultPosition.x,
+      y: props.defaultPosition.y,
     }
+
+    this.x = props.defaultPosition.x
+    this.y = props.defaultPosition.y
   }
 
   handleStart = (ev, b) => {
-    this.lastX = b.lastX
-    this.lastY = b.lastY
-    this.props.onStart()
+    const {x, y} = this.state
+    this.lastX = b.lastX - x
+    this.lastY = b. lastY - y
+    this.props._init()
+    this.props.onStart(ev, createCoreData(b, {x, y}))
   }
 
   handleDrag = (ev, b) => {
-    const dragX = b.lastX - this.lastX + this.props.position.x
-    const dragY = b.lastY - this.lastY + this.props.position.y
-    const {x, y} = this.props.onDrag(dragX, dragY)
+    const dragX = b.lastX - this.lastX
+    const dragY = b.lastY - this.lastY
+    const {x, y} = this.props._calc(dragX, dragY)
     this.setState({ x, y })
+
+    this.props.onDrag(ev, createCoreData(b, {
+      originX: dragX,
+      originY: dragY,
+      x,
+      y,
+    }))
   }
 
-  handleStop = () => {
+  handleStop = (ev, b) => {
     const {x, y} = this.state
-    this.props.handleChange({x, y})
-    this.props.onStop()
+    this.props._stop()
+    this.props.onStop(ev, createCoreData(b, {x, y}))
   }
 
   render() {
@@ -63,7 +82,12 @@ export default class DraggableChild extends React.Component {
         onStart={this.handleStart}
         position={{ x, y }}
       >
-        {React.cloneElement(this.props.children, { style, className })}
+        {React.cloneElement(this.props.children, {
+          style,
+          className,
+          ['data-x']: x,
+          ['data-y']: y,
+        })}
       </DraggableCore>
     )
   }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+export const noop = () => {}
+
 export function unique(array, compare = (a, b) => a === b) {
   const result = []
   for (let i = 0, len = array.length; i < len; i++) {
@@ -17,7 +19,21 @@ export const checkArrayWithPush = (target, key, value) => {
   }
 }
 
+export const createCoreData = ({node, deltaX, deltaY}, {originX, originY, x, y}) => {
+  return {
+    node,
+    deltaY,
+    deltaX,
+    originX: originX || x,
+    originY: originY || y,
+    x,
+    y,
+  }
+}
+
 export default {
+  noop,
   unique,
+  createCoreData,
   checkArrayWithPush,
 }

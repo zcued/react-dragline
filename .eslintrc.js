99c70545f6015cc337023f52a943ff7cc379c89a
huangdehao <huangdehao@zcool.com.cn>
2019-01-27 14:48:08 +0800

    fix: :wrench: object-surly-spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": "eslint-config-zcool",
   "parser": "babel-eslint",
   "rules": {
+    "object-curly-spacing": ["error", "always"],
     "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
     "no-else-return": [0],
     "padding-line-between-statements": [0],
